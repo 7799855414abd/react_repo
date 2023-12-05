737c05472ec15f52836e29135dcee3b465d58e42
Kris <kris051199@gmail.com>
2023-12-05 12:10:26 +0530

    last use ffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,54 @@
 import React,{useState,useEffect} from "react";
 import "./index.css";
 
-const App = () =>{
-  const [count,setCount] = useState(0);
- 
 
-   useEffect(()=> {
-      console.log("Hello iam from useEffect",count)   
-   },[count])
+const url = "https://jsonplaceholder.typicode.com/posts/";
+const App = () =>{
+  const [drinksdata, setDrinksdata] = useState([]);
+  const [searchTerm,setSearchterm] = useState(1);
+  
+  
+  const fetchData = async(apiUrl) => {
+    const response = await fetch(apiUrl);
+    const data = await response.json()
+    setDrinksdata(data)
+  }
+
+  useEffect(()=> {
+    const correctUrl = `${url}${searchTerm}`
+    fetchData(correctUrl)
+    
+    
+  },[searchTerm])
+
+
   
   return(
          <div style = {{"textAlign":"center"}}>
+             <form>
+               <input type = "text"
+                      name = "search"
+                      id = "search"
+                      placeholder="search something new ..."
+                      onChange={(e) => setSearchterm(e.target.value)}
+               />
+             </form>
+
+             <hr/>
+             <ul className="unorder-list-cont">
+              {
+                drinksdata.map((eachDrink) => {
+                  const {id, title, body } = eachDrink;
+                  return (
+                    <li key = {id} className="listelement">
+                      <h1>{title}</h1>
+                      <p>{body}</p>
+                    </li>
+                  )
+         })
+              }
+             </ul>
              
-             <h1>Learn Use Effect</h1>
-             <h3  >{count}</h3>
-             <button onClick = {()=>{setCount(count+1)}}>XXX</button>
          </div>
   )
   
